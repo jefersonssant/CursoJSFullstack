99b586302217984824c43762803f21ea6ccf19fd
jefersonssant <jefss@outlook.com>
2023-11-02 01:03:08 -0300

    Exercício de lógica
    Com function e arrow function.

--- Aula 41/index.js
+++ Aula 41/index.js
@@ -8,3 +8,17 @@ const max2 = (x, y) => x > y ? x : y;
 // quando só tem uma linha de bloco de código pode eliminar as chaves e remover a palavra return
 
 console.log(max2(10, 20));
+
+// Escreva uma função chamada ePaisagem
+// que recebe dois argumentos, largura e altura
+// de uma imagem (number)
+// Retorne true se a imagem estiver no modo paisagem
+
+// function ePaisagem (largura, paisagem) {
+//     return largura > paisagem; /*? true : false; neste caso pode-se omitir true e false*/
+// }
+// com arrow function:
+
+const ePaisagem = (largura, altura) => largura >= altura;
+
+console.log(ePaisagem(1920, 1920));
