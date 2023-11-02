e842667e7299eb8c39794f9a7a01c6c90a7198a2
jefersonssant <jefss@outlook.com>
2023-11-02 00:45:26 -0300

    Exercício de lógica
    Com function e arrow function.

---
+++ Aula 41/index.js
@@ -0,0 +1,10 @@
+// Exercício de Lógica de Programação 01 - escreva uma função que recebe 2 número e retorne o maior deles
+
+// function max (x, y) {
+//     return x > y ? x : y
+// }
+
+const max2 = (x, y) => x > y ? x : y;
+// quando só tem uma linha de bloco de código pode eliminar as chaves e remover a palavra return
+
+console.log(max2(10, 20));
