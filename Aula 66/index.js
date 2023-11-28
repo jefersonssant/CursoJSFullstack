add6a3a4f9e75d665310515eed1df2941e125381
jefersonssant <jefss@outlook.com>
2023-11-28 15:41:36 -0300

    Array: reduce()

---
+++ Aula 66/index.js
@@ -0,0 +1,49 @@
+// Array - Reduce: reduzir um array a um único elemento, também recebe uma função de callback
+
+// Some todos os números (Reduce)
+// Retorne um array com os valores pares (filter)
+// Retorne um array com o dobro dos valores (Map)
+const numeros = [5, 50, 80, 1, 2, 3, 5, 8, 7, 11, 15, 22, 27];
+
+// const total = numeros.reduce(function(acumulador, valor, indice, array){
+//     acumulador += valor;
+//     return acumulador;
+// }, 0/*Valor inicial do acumulador*/);
+
+// const total = numeros.reduce(function(acumulador, valor, indice, array){
+//     if (valor % 2 === 0) acumulador.push(valor);
+//     return acumulador;
+// }, []); //Ideal fazer com filter
+
+// const total = numeros.reduce(function(acumulador, valor){
+//     acumulador.push(valor * 2);
+//     return acumulador;
+// }, []); //Ideal fazer com map
+
+const total = numeros.reduce(function(acumulador, valor){
+    if (valor % 2 !== 0) {
+        acumulador += valor;
+    }
+
+    return acumulador
+}, 0);
+
+// console.log(total); 
+
+//Retorne a pessoa mais velha
+
+const pessoas = [
+    {nome: 'Luiz', idade: 62},
+    {nome: 'Maria', idade: 23},
+    {nome: 'Eduardo', idade: 55},
+    {nome: 'Letícia', idade: 19},
+    {nome: 'Rosana', idade: 32},
+    {nome: 'Wallace', idade: 47}
+];
+
+const maisVelha = pessoas.reduce(function(acumulador, valor) {
+    if (acumulador.idade > valor.idade) return acumulador;
+    return valor;
+});
+
+console.log(maisVelha);
