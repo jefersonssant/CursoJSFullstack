d77343bb41c3cd6a8862a9d3103d0665548c37e4
jefersonssant <jefss@outlook.com>
2023-11-28 22:16:22 -0300

    forEach()

---
+++ Aula 68/index.js
@@ -0,0 +1,20 @@
+// forEach() - forma de iterar no array, só está disponível dentro de arrays
+
+const a1 = [10 ,20, 30, 40, 50, 60, 70, 80, 90];
+
+// for (let valor of a1) {
+//     console.log(valor);
+// }
+
+// a1.forEach(function(valor, indice, array) {
+//     console.log(valor, indice, array);
+// });
+
+let total = 0;
+
+a1.forEach(valor => {
+    total += valor;
+});
+
+console.log(total);
+
