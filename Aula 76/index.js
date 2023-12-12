c0906a4728e7bef072f0fb774b63d9d860f71785
jefersonssant <jefss@outlook.com>
2023-12-12 20:05:46 -0300

    Algoritmo -> validando CPF
    Exercício completo.

--- Aula 76/index.js
+++ Aula 76/index.js
@@ -15,7 +15,51 @@ Se o número dígito for maior que 9, considera-se 0.
 11 - (284 % 11) = 2 (Primeiro dígito)
  */
 
-let cpf = '705.484.450-52';
-let cpfLimpo = cpf.replace(/\D+/g, '');
-cpfArray = Array.from(cpfLimpo);
-console.log(cpfArray.reduce((ac, val) => ac + Number(val), 0));
+// let cpf = '705.484.450-52';
+// let cpfLimpo = cpf.replace(/\D+/g, '');
+// cpfArray = Array.from(cpfLimpo);
+// console.log(cpfArray.reduce((ac, val) => ac + Number(val), 0));
+
+function ValidaCPF(cpfEnviado) {
+  Object.defineProperty(this, 'cpfLimpo', {
+    enumerable: true,
+    get: function() {
+      return cpfEnviado.replace(/\D+/g, '');
+    }
+  });
+}
+ValidaCPF.prototype.valida = function() {
+  if (typeof this.cpfLimpo === 'undefined') return false;
+  if (this.cpfLimpo.length !== 11) return false;
+  if (this.isSequencia()) return false;
+
+  const cpfParcial = this.cpfLimpo.slice(0, -2);
+  const digito1 = this.criaDigito(cpfParcial);
+  const digito2 = this.criaDigito(cpfParcial + digito1);
+  const novoCpf = cpfParcial + digito1 + digito2;
+  console.log(novoCpf);
+  
+  return novoCpf === this.cpfLimpo;
+}
+ValidaCPF.prototype.criaDigito = function(cpfParcial) {
+  const cpfArray =  Array.from(cpfParcial);
+  let regressivo = cpfArray.length + 1;
+  const total = cpfArray.reduce((ac, val) => {
+    ac += (regressivo * Number(val));
+    regressivo --;
+    return ac;
+  }, 0);
+  
+  const digito = 11 - (total % 11);
+  return digito > 9 ? '0' : String(digito);
+  console.log(digito);
+
+}
+
+ValidaCPF.prototype.isSequencia = function() {
+  const sequencia = this.cpfLimpo[0].repeat(this.cpfLimpo.length);
+  return sequencia === this.cpfLimpo;
+}
+
+const cpf = new ValidaCPF('070.987.720-03');
+console.log(cpf.valida());
