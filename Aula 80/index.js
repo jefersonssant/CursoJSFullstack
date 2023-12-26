5669f77edbaf77f8a736ac00f7a1e9cef9a1dd05
jefersonssant <jefss@outlook.com>
2023-12-26 10:32:09 -0300

    Criando Classes
    Diferenças entre Constructor Functions

---
+++ Aula 80/index.js
@@ -0,0 +1,23 @@
+// Criando classes
+
+class Pessoa {
+  constructor(nome, sobrenome) {
+    this.nome = nome;
+    this.sobrenome = sobrenome;
+  }
+
+  falar() {
+    console.log(`${this.nome} está falando`);
+  }
+  
+  comer() {
+    console.log(`${this.nome} está comendo`);
+  }
+  
+  beber() {
+    console.log(`${this.nome} está bebendo`);
+  }
+}
+
+const p1 = new Pessoa("Jef", "Santos");
+console.log(p1);
