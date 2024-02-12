cd39b0b3db83fe0c7f5ffdb22058ba7165258280
jefersonssant <jefss@outlook.com>
2024-02-12 12:16:19 -0300

    Métodos úteis para Promises
    Promise.all Promise.race Promise.resolve Promise.reject

---
+++ Aula 87/index.js
@@ -0,0 +1,55 @@
+// Métodos úteis para Promises
+
+function rand(min, max) {
+  min *= 1000;
+  max *= 1000;
+  return Math.floor(Math.random() * (max - min) + min);
+}
+
+function esperaAi(msg, tempo) {
+  return new Promise((resolve, reject) => {
+    if(typeof msg !== 'string') { reject('CAÍ NO ERRO')
+    return; 
+  }
+    setTimeout(() => {
+      resolve(msg.toUpperCase() + ' - Passei na promise');
+    }, tempo);
+  });
+
+  
+}
+
+// Promise.all Promise.race Promise.resolve Promise.reject
+
+// const promises = [
+//   //'Primeiro valor',
+//   esperaAi('Promise 1', rand(1, 5)),
+//   esperaAi('Promise 2', rand(1, 5)),
+//   esperaAi('Promise 3', rand(1, 5)),
+//   // esperaAi(1000),
+//   //'Outro valor'
+// ]
+
+// Promise.race(promises)
+// .then(function(valor){
+//   console.log(valor);
+// })
+// .catch(function(erro){
+//   console.log(erro);
+// });
+
+function baixaPagina(){
+  const emCache = true;
+
+  if(emCache) {
+    return Promise.reject('Página em cache');
+  } else {
+    return esperaAi('Baixei a página', 3000);
+  }
+}
+
+baixaPagina()
+.then(dadosPagina => {
+  console.log(dadosPagina);
+})
+.catch(e => console.log('ERRO', e))
