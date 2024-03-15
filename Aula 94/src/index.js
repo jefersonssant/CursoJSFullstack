5c61a8f4e66a928d2ed8d1aea58aaf9d127b290e
jefersonssant <jefss@outlook.com>
2024-03-15 10:32:48 -0300

    Aula 94 Import e Export

---
+++ Aula 94/src/index.js
@@ -0,0 +1,15 @@
+// import { nome, sobrenome, idade, soma, Pessoa } from './modulo1';
+
+// // const nome = 'Rafael';
+
+// const P1 = new Pessoa('Rafael', 'Macedo');
+
+// console.log(nome, sobrenome, idade);
+// console.log(P1);
+// console.log(soma(5, 5));
+
+// import * as meuModulo from './modulo1';
+// console.log(meuModulo);
+
+import qualquerNome from './modulo1';
+console.log(qualquerNome(2, 5));
---
+++ Aula 94/src/modulo1.js
@@ -0,0 +1,15 @@
+export const nome = 'Jeferson';
+export const sobrenome = 'Santos';
+export const idade = 28;
+const cpf = 1515261515;
+
+export default function soma(x, y) {
+  return x + y;
+}
+
+// export class Pessoa {
+//   constructor(nome, sobrenome) {
+//     this.nome = nome;
+//     this.sobrenome = sobrenome;
+//   }
+// }
