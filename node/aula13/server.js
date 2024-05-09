82ea8e839ffe965dc998f44b167c9d14f6edf2a7
jefersonssant <jefss@outlook.com>
2024-05-08 22:25:29 -0300

    Express - middlewares

---
+++ node/aula13/server.js
@@ -0,0 +1,32 @@
+
+const express = require('express')
+const app = express()
+const routes = require('./routes')
+const path = require('path')
+const {middlewareGlobal} = require('./src/middlewares/middleware.js')
+
+// http://facebook.com/profiles/123152?
+// campanha=googleads &
+// nome_campanha=seila
+
+app.use(
+  express.urlencoded({
+    extended: true
+  })
+)
+
+app.use(express.static(path.resolve(__dirname, 'public')))
+
+app.set('views', path.resolve(__dirname, 'src', 'views'))
+app.set('views engine', 'ejs')
+
+// Nossos próprios middlewares
+app.use(middlewareGlobal)
+
+app.use(routes)
+
+app.listen(3000, () => {
+  console.log('Acessar http://localhost:3000');
+  console.log('Servidor executando na porta 3000');
+})
+
